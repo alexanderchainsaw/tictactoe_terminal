20bbe5fa07081b6b2b4a3e13f7862529fa7ee421
alexanderchainsaw <alexanderkolesov.contact@gmail.com>
2023-06-17 23:33:01 +0300

    smarter ai
    upgraded ai to choose better moves

--- ai_opponent.py
+++ ai_opponent.py
@@ -3,19 +3,25 @@ import random
 
 def ai_move(board, moves, playing_as, playing_vs):
     """ Priority in choice of moves:
-        Win > Disrupt enemy win > Random move from available moves """
+        Win > Disrupt enemy win > Random secondary move > Random move from available """
     wins = [board[:5:2], board[6:11:2], board[12::2], board[::6],
             board[2::6], board[4::6], board[::8], board[4:13:4]]
-    win_move = ''
-    break_move = ''
+    win_moves = ''
+    break_moves = ''
+    secondary_move = ''.join(''.join(i for i in x if i in '123456789')
+                             for x in wins if playing_as in x and playing_vs not in x)
     for x in wins:
         if x.count(playing_as) == 2:
-            win_move += ''.join(i for i in x if i in '123456789')
+            win_moves += ''.join(i for i in x if i in '123456789')
         if x.count(playing_vs) == 2:
-            break_move += ''.join(i for i in x if i in '123456789')
-    if win_move:
-        return win_move[0]
-    elif break_move:
-        return break_move[0]
+            break_moves += ''.join(i for i in x if i in '123456789')
+    if win_moves:
+        return win_moves[0]
+    elif break_moves:
+        return break_moves[0]
+    elif secondary_move:
+        return '5' if '5' in secondary_move else random.choice(secondary_move)
     else:
         return random.choice(moves)
+
+
